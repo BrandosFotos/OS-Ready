20fe0df5769e25f7af50db7a855758abc8574dd5
BrandosFotos <bjwhite97@gmail.com>
2025-04-13 18:13:08 -0700

    feat: Added more apps to windows installation script

--- Win/install_apps.ps1
+++ Win/install_apps.ps1
@@ -1,11 +1,10 @@
 # install.ps1
 
 # Install Chocolatey (if not installed)
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 if (-not (Get-Command choco -ErrorAction SilentlyContinue)) {
-    Write-Output "Installing Chocolatey..."
-    Set-ExecutionPolicy Bypass -Scope Process -Force
-    [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
-    iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+    Write-Host "Chocolatey installation failed. Exiting script."
+    exit 1
 }
 
 # Install apps
@@ -15,4 +14,9 @@ choco install googlechrome -y
 choco install 7zip -y
 choco install firefox -y
 choco install bambustudio -y
-choco install geforce-game-ready-driver -y
+choco uninstall geforce-game-ready-driver -y
+choco install microsoft-teams.install -y
+choco install epicgameslauncher -y
+choco install discord -y
+choco install steam -y
+choco install geforce-experience -y
